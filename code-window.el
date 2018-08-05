72e50de9f05a1f6775dee1d6d4c025d2406f8786
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2018-08-05 16:26:31 +0200

    Started on the various edit cases

--- code-window.el
+++ code-window.el
@@ -5,11 +5,6 @@
 
 (setq cw/code-window-buffer-name "code-window-buffer")
 
-(defun cw/create-code-window-buffer ()
-  (unless cw/buffer
-	(setq cw/buffer (get-buffer-create cw/code-window-buffer-name))
-	(add-hook 'after-change-functions #'cw/update-buffer-hook)))
-
 (setq cw/windows '())
 
 (cl-defstruct cw/window buffer region-begin region-end)
@@ -22,34 +17,36 @@
   (setq cw/windows '()))
 
 (defun cw/update-buffer ()
+  (message "Updating buffer")
   (with-current-buffer cw/buffer
 	(erase-buffer)
 	(-each cw/windows
 	  (lambda (item)
 		(cw/get-text-from-window item)))))
 
-(defun cw/update-buffer-hook (beginning end length)
+(defun cw/update-buffer-hook (beginning end length-before-edit)
   (let ((window-being-changed
 		 (-first (lambda (window) (equal (cw/window-buffer window) (current-buffer))) cw/windows)))
-	(message "Some editing is happening")
-	(unless window-being-changed
-	  (let ((window-begin (cw/window-region-begin window-being-changed))
-			(window-end (cw/window-region-end window-being-changed)))
+	(when window-being-changed
+	  (let* ((window-begin (cw/window-region-begin window-being-changed))
+			(window-end (cw/window-region-end window-being-changed))
+			(length-after-edit (- window-end window-begin))
+			(length-diff (- length-after-edit length-before-edit)))
 		(cond
-		 ;; if the region being changed starts before our window, we assume all the content coming after should push our window further down
-		 ((< start window-begin) )
-
-		 ;; if region being changed is inside our window, might need to extend it?
-		 ((and (> start window-begin)
+		 ((< end window-begin)
+		  (message "Change was above and inside the window")
+		  (setf (cw/window-region-begin window-being-changed) (+ window-begin length-diff)))
+		 ((and (> start window-start)
 			   (< end window-end))
-		  )
-
-		 ;; Region being changed is partially inside and partially below our window. Might need to extend the window.
-		 ((and (> start window-begin)
-			   (> end window-end))
-		  ))
+		  (message "Change was inside window")
+		  (setf (cw/window-region-begin window-being-changed) (+ window-begin length-diff))))
 
 		(cw/update-buffer)))))
+
+(defun cw/create-code-window-buffer ()
+  (unless cw/buffer
+	(setq cw/buffer (get-buffer-create cw/code-window-buffer-name))
+	(add-hook 'after-change-functions #'cw/update-buffer-hook)))
 
 (defun cw/create-window ()
   (interactive)
